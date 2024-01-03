8ed3d1c630ea0f4aaab7fcbb5be3f156757cdfd6
unknown <asolkarprutvhi7@gmail.com>
2024-01-03 16:18:07 +0530

    first

--- src/app/Screens/Hero.js
+++ src/app/Screens/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Style.css";
-import courseData from "./data/course.json";
+import courseData from "../Screens/Data/Course.json";
 const Hero = () => {
   const { instructor, course, about_instructor, testimonial } = courseData;
 
